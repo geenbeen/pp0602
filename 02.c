990a0cbe5ecb0530f147e700451b71516abb0cc4
geenbeen <43523710+geenbeen@users.noreply.github.com>
2018-09-27 13:56:43 -0500

    Update 02.c

--- 02.c
+++ 02.c
@@ -4,11 +4,14 @@ int main(void) {
 	printf("Enter a 24 hour time: ");
 	int hour;
 	int minute;
-	scanf("%d:%d", &hour, &minute);
+	scanf("%d:%2d", &hour, &minute);
 	if (hour>12) {
-		printf("%d:%d PM", hour-12, minute);
-	} else {
-		printf("%d:%d AM", hour, minute);
+		printf("%d:%-.2d PM", hour-12, minute);
+	}
+	else if(hour==12) {
+		printf("%d:%-.2d PM", 12, minute);
+	}
+	else {
+		printf("%d:%-.2d AM", hour, minute);
 	}
 }
-		
